d44a2f74b00b51db19791bc32ac717d7635d566c
DevFrog92 <yongsu1481@gmail.com>
2022-01-28 17:15:21 +0900

    add vue file upload component

---
+++ vue-upload/vue-upload-frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <image-upload />
+  </div>
+</template>
+
+<script>
+import ImageUpload from './components/ImageUpload.vue'
+export default {
+  name: 'App',
+  components: {
+    ImageUpload
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ vue-upload/vue-upload-frontend/src/components/ImageUpload.vue
@@ -0,0 +1,46 @@
+<template>
+  <section>
+    <img ref="preview" src="" alt="" class="preview-image">
+    <input ref="thumbnail" type="file" name="image" accept=".jpg, .jpeg, .png" @change="onChange">
+    <button @click="onUploadImage">upload</button>
+  </section>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      image:null
+    }
+  },
+  methods: {
+    onChange(e) {
+      this.image = e.target.files[0]
+      const previewImage = window.URL.createObjectURL(this.image)
+
+      this.$refs.preview.src = previewImage
+    },
+    onUploadImage(){
+      const formData = new FormData()
+      formData.append('thumbnail', this.image)
+      for(let key of formData.entries()){
+        console.log(key[0],key[1])
+      }
+      console.log(formData)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.preview-image {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 500px;
+  height: 500px;
+  border: 1px solid;
+}
+
+</style>
