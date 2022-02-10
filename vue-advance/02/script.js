18434edcc6e073ca4afaa364bba354df0f53eb3e
DevFrog92 <yongsu1481@gmail.com>
2022-02-10 14:06:30 +0900

    add vue market project

---
+++ vue-advance/02/script.js
@@ -0,0 +1,44 @@
+// 로딩 및 vVue가 전역 변수로 정의됬는지 확인
+// 내부 assertion이 false라면 error msg를 출력하지만, true면 아무 동작하지 앉는다.
+console.assert(typeof Vue !== 'undefined');
+
+const items = [
+  { name:"연필", price:300, quantity:0 },
+  { name:"공책", price:400, quantity:0 },
+  { name:"지우개", price:500, quantity:0 },
+]
+
+const vm = new Vue({
+  el: "#app",
+  data: {
+    items: items,
+    loggedInButton: "로그인 후에 구매 가능합니다."
+  },
+  computed: {
+    // numberWithDelimiter: function() {
+    // javascript의 toLocaleString으로
+    // numberWithDelimiter filter와 동일한 기능을 수행할 수 있다.
+    //   return function(value) {
+    //     return value.toLocaleString()
+    //   }
+    // },
+    totalPrice: function() {
+      return this.items.reduce(function(sum, item){
+        return sum + (item.price * item.quantity)
+      }, 0)
+    },
+    totalPriceWithTax() {
+      // ES5
+      return Math.floor(this.totalPrice * 1.10)
+    }
+  },
+  filters:{
+    numberWithDelimiter: function(value) {
+      if(!value) {
+        return '0'
+      }
+
+      return value.toString().replace(/(\d)(?=(\d{3})+$)/g,'$1,')
+    }
+  }
+});
