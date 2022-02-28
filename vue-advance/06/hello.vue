4dc608a1c7e5dc9fc91921ef5c011ff0c2be56da
DevFrog92 <yongsu1481@gmail.com>
2022-02-28 22:43:58 +0900

    add css module

---
+++ vue-advance/06/hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <p class="message">{{msg}}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return  {
+      msg: "This is vue component"
+    }
+  }
+}
+</script>
+
+<style module>
+.message {
+  color: #42b983;
+}
+</style>
