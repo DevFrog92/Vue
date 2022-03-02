adeb1da8c71e34d0cfea0634981d6fda7dafa414
DevFrog92 <yongsu1481@gmail.com>
2022-03-02 16:26:06 +0900

    add vuex library

---
+++ vue-advance/07/store.vue
@@ -0,0 +1,17 @@
+<template>
+  <div> store</div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Vuex from "vuex"
+
+Vue.use(Vuex)
+
+const store = new Vuex.store({})
+console.log(store)
+</script>
+
+<style>
+
+</style>
