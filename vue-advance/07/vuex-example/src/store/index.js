6f528e644b30dd18897910454d1e472bafd65b7b
DevFrog92 <yongsu1481@gmail.com>
2022-03-02 23:01:20 +0900

    add label generate

--- vue-advance/07/vuex-example/src/store/index.js
+++ vue-advance/07/vuex-example/src/store/index.js
@@ -9,15 +9,69 @@ const store = new Vuex.Store({
       {
         id: 1,
         name: "Javascript study",
+        labelIds: [1, 2],
         done: false
       },
       {
         id: 2,
         name: "Typescript study",
+        labelIds: [1, 3],
         done: true
       }
-    ]
-  }
+    ],
+    labels: [
+      {
+        id: 1,
+        text: "study"
+      },
+      {
+        id: 2,
+        text: "javascript"
+      },
+      {
+        id: 3,
+        text: "typescript"
+      }
+    ],
+    nextTaskId: 3,
+    nextLabelId:4
+  },
+  mutations: {
+    addTask(state, {
+      name,
+      labelIds
+    }) {
+      state.tasks.push({
+        id: state.nextTaskId,
+        name,
+        labelIds,
+        done: false
+      })
+
+      state.nextTaskId++
+    },
+    toggleTaskStatus(state, {
+      id
+    }) {
+      const filtered = state.tasks.filter(task => {
+        return task.id === id
+      })
+
+      filtered.forEach(task => {
+        task.done = !task.done
+      })
+    },
+    addLabel(state, {
+      text
+    }){
+      state.labels.push({
+        id: state.nextLabelId,
+        text
+      })
+
+      state.nextLabelId++
+    }
+  },
 })
 
 export default store
