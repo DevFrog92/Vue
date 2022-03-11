0f71ae3d1bbcd7ebc114db026d1c49ec3ed5ec69
DevFrog92 <yongsu1481@gmail.com>
2022-03-11 16:54:04 +0900

    add signup GA trigger

--- vue-advance/vue-gtag-test/src/components/Signup.vue
+++ vue-advance/vue-gtag-test/src/components/Signup.vue
@@ -3,16 +3,36 @@
     <form @submit.prevent>
       <label for="id">ID</label>
       <input
+        v-model="id"
         type="text"
         class="id"
       >
+      <button
+        type="button"
+        @click="signup"
+      >
+        signup
+      </button>
     </form>
   </section>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      id: '',
+    };
+  },
+  methods: {
+    signup() {
+      this.$gtag.event('sign_up', {
+        signup_status: true,
+      });
 
+      this.id = '';
+    },
+  },
 };
 </script>
 
