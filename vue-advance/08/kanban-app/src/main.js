06f3759dd9c6acdf30013e18a10f7aaf11e29213
DevFrog92 <yongsu1481@gmail.com>
2022-03-27 15:04:44 +0900

    🔧  add devtools prevent logic in production mode

--- vue-advance/08/kanban-app/src/main.js
+++ vue-advance/08/kanban-app/src/main.js
@@ -4,7 +4,9 @@ import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+Vue.config.devtools = process.env.NODE_ENV === "production" ? false : true;
 
+console.log(process.env.NODE_ENV);
 new Vue({
   router,
   store,
