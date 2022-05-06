7707e95d367bedd6791e496adaf07cb453787fdb
DevFrog92 <yongsu1481@gmail.com>
2022-05-06 18:50:09 +0900

    🌱 update position component

--- vue-toy-component/src/views/Position.vue
+++ vue-toy-component/src/views/Position.vue
@@ -1,9 +1,33 @@
 <template>
-  <section>this is position test</section>
+  <section class="container">
+    <div class="box sticky"></div>
+    <div class="box fixed"></div>
+  </section>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+.container {
+  width: 100vw;
+  height: 300vh;
+}
+
+.box {
+  width: 100px;
+  height: 100px;
+  border: 2px solid;
+}
+
+.sticky {
+  position: sticky;
+  top: 0;
+}
+
+.fixed {
+  position: fixed;
+  top: 0;
+}
+</style>
