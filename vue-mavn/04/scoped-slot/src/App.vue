ed3abae82985a23eb09023d2a0878771e76170a3
DevFrog92 <yongsu1481@gmail.com>
2022-05-15 22:26:40 +0900

    🌱  add scop slot example

---
+++ vue-mavn/04/scoped-slot/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <travel-list :items="cities">
+      <template slot-scope="{items}">
+        <p v-for="item in items" :key="item">{{ item }}</p>
+      </template>
+    </travel-list>
+  </div>
+</template>
+
+<script>
+import TravelList from "./components/TravelList.vue"
+export default {
+  name: 'App',
+  components: {
+    TravelList
+  },
+  data(){
+    return {
+      cities: ["Paris", "Seoul", "Budapest", "Seattle", 'Japan']
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
