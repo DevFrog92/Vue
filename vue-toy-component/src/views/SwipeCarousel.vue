297e5a3738f1c06d895e65a8da13a3593b4363c4
DevFrog92 <yongsu1481@gmail.com>
2022-07-20 23:43:51 +0900

    ✨ Implement swipe carousel component

--- vue-toy-component/src/views/SwipeCarousel.vue
+++ vue-toy-component/src/views/SwipeCarousel.vue
@@ -1,20 +1,25 @@
 <template>
   <div class="swipe-carousel-wrapper">
     <h1>Swipe carousel</h1>
-    <ul class="swipe-carousel">
-      <li class="swipe-carousel-item">
-        <img src="../assets/images/slider_01.jpeg" alt="" draggable="false" />
-      </li>
-      <li class="swipe-carousel-item">
-        <img src="../assets/images/slider_02.jpeg" alt="" draggable="false" />
-      </li>
-      <li class="swipe-carousel-item">
-        <img src="../assets/images/slider_03.jpeg" alt="" draggable="false" />
-      </li>
-      <li class="swipe-carousel-item">
-        <img src="../assets/images/slider_04.jpeg" alt="" draggable="false" />
-      </li>
-    </ul>
+    <div class="swipe-carousel-container">
+      <ul class="swipe-carousel">
+        <li class="swipe-carousel-item">
+          <img src="../assets/images/1.jpeg" alt="" draggable="false" />
+        </li>
+        <li class="swipe-carousel-item">
+          <img src="../assets/images/2.jpeg" alt="" draggable="false" />
+        </li>
+        <li class="swipe-carousel-item">
+          <img src="../assets/images/3.jpeg" alt="" draggable="false" />
+        </li>
+        <li class="swipe-carousel-item">
+          <img src="../assets/images/4.jpeg" alt="" draggable="false" />
+        </li>
+        <li class="swipe-carousel-item">
+          <img src="../assets/images/5.jpeg" alt="" draggable="false" />
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -24,6 +29,10 @@ export default {
     return {
       items: null,
       container: null,
+      dragging: false,
+      currentIndex: 0,
+      prevTranslateX: 0,
+      startPosition: 0,
     };
   },
   mounted() {
@@ -44,15 +53,11 @@ export default {
           config,
         );
 
-        element.addEventListener(
-          'touchend',
-          () => this.touchEnd(index),
-          config,
-        );
+        element.addEventListener('touchend', () => this.touchEnd(), config);
 
         element.addEventListener(
           'touchmove',
-          () => this.touchMove(index),
+          event => this.touchMove(event),
           config,
         );
 
@@ -62,30 +67,73 @@ export default {
           config,
         );
 
-        element.addEventListener('mouseup', () => this.touchEnd(index), config);
+        element.addEventListener('mouseup', () => this.touchEnd(), config);
+
+        element.addEventListener('mouseleave', () => this.touchEnd(), config);
 
         element.addEventListener(
-          'mouseleave',
-          () => this.touchEnd(index),
+          'mousemove',
+          event => this.touchMove(event),
           config,
         );
       });
     },
     touchStart(event, index) {
       console.log(event);
-      console.log(this.getMousePositionX(event));
+      this.currentIndex = index;
+      this.startPosition = this.getMousePositionX(event);
+      this.dragging = true;
+
+      this.swipeAnimation();
       this.container.classList.add('grabbing');
+    },
+    swipeAnimation() {
+      this.setCarouselPosition();
+      if (this.dragging) {
+        this.swipeAnimationId = requestAnimationFrame(this.swipeAnimation);
+      }
+    },
+    setCarouselPosition() {
+      this.container.style.transform = `translate3d(${this.currentTranslate}px, 0, 0)`;
     },
     getMousePositionX(event) {
       return event.type.includes('mouse')
         ? event.clientX
         : event.touches[0].clientX;
     },
-    touchEnd(index) {
+    touchEnd() {
+      this.dragging = false;
+      cancelIdleCallback(this.swipeAnimationId);
+
+      const moveBy = this.currentTranslate - this.prevTranslateX;
+
+      if (moveBy < -100 && this.currentIndex < 4) {
+        this.currentIndex += 1;
+      }
+
+      if (moveBy > 100 && this.currentIndex > 0) {
+        this.currentIndex -= 1;
+      }
+
+      this.setPositionByIndex();
       this.container.classList.remove('grabbing');
     },
+    setPositionByIndex() {
+      this.currentTranslate = this.currentIndex * -400;
+      this.prevTranslateX = this.currentTranslate;
+      this.setCarouselPosition();
+    },
     translateItem() {},
-    touchMove() {},
+    touchMove(event) {
+      if (!this.dragging) {
+        return;
+      }
+
+      const currentPosition = this.getMousePositionX(event);
+      this.currentTranslate =
+        this.prevTranslateX + currentPosition - this.startPosition;
+      console.log('currentTranslate', this.currentTranslate);
+    },
   },
 };
 </script>
@@ -94,8 +142,9 @@ export default {
 .swipe-carousel-wrapper {
   width: 100vw;
   height: 100vh;
-  background-color: rgb(18, 100, 64);
+  background-color: rgb(0, 0, 0);
   display: flex;
+  justify-content: center;
   flex-direction: column;
   align-items: center;
 
@@ -103,24 +152,35 @@ export default {
     color: #fff;
   }
 
-  .swipe-carousel {
-    width: 660px;
+  .swipe-carousel-container {
+    width: 400px;
     height: 500px;
     overflow: hidden;
-    border: 1px solid #fff;
+    margin-top: 40px;
+  }
+
+  .swipe-carousel {
+    height: 500px;
+    width: calc(400 * 5px);
+    display: flex;
+    align-items: center;
+    transition: all 0.25s linear;
+    cursor: grab;
 
     &.grabbing {
       border-color: red;
+      cursor: grabbing;
     }
 
     &-item {
       height: 100%;
-      width: 100%;
+      width: 400px;
 
       img {
         display: block;
-        height: 100%;
+        height: 110%;
         width: 100%;
+        transform: translateY(-10%);
       }
     }
   }
