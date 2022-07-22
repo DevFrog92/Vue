0c49f4c6e85ad763d0224fe930e3dface1bb91d1
DevFrog92 <yongsu1481@gmail.com>
2022-07-22 18:41:37 +0900

    🌱 Add sms share button

--- vue-toy-component/src/views/Button.vue
+++ vue-toy-component/src/views/Button.vue
@@ -4,6 +4,7 @@
     <Button class="primary">Button</Button>
     <Button class="primary">Button</Button>
     <Button class="primary">Button</Button>
+    <button @click="shareSMS">sms</button>
   </div>
 </template>
 
@@ -23,6 +24,23 @@ export default {
   // eslint-disable-next-line vue/no-shared-component-data
   data: {
     number: 0,
+  },
+  methods: {
+    shareSMS() {
+      let linkString = 'sms:';
+      const userAgent = navigator.userAgent.toLocaleLowerCase();
+
+      if (userAgent.search('android') > -1) {
+        linkString += `?body=hello`;
+      } else if (
+        userAgent.search('iphone') > -1 ||
+        userAgent.search('ipad') > -1
+      ) {
+        linkString += `&body=hello`;
+      }
+
+      location.href = linkString;
+    },
   },
 };
 </script>
